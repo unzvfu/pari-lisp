d64a6d4239e662a84fb030a3b12372d726f7e5fc
Hamish Ivey-Law <hamish.ivey.law@gmail.com>
2014-05-09 13:00:35 +0200

    Added copyright statements to each file.

--- pari-scm.c
+++ pari-scm.c
@@ -1,4 +1,21 @@
 /*
+ * Copyright 2014 Hamish Ivey-Law
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * -*- compile-command: "gcc -o pari-scm pari-scm.c `pkg-config --cflags --libs guile-2.0` -lpari -lgmp" -*-
  *
  * -*- compile-command: "gcc -L ~/src/pari-git/GP.dbg/lib -I ~/src/pari-git/GP.dbg/include -o pari-scm pari-scm.c `pkg-config --cflags --libs guile-2.0` -lpari -lgmp" -*-

412a4ce252dc71adb9d13567cd944a9240afde62
Hamish Ivey-Law <hamish.ivey.law@gmail.com>
2014-06-05 00:25:37 +0200

    Added more hand-made examples of prototypes.

--- pari-racket.rkt
+++ pari-racket.rkt
@@ -114,11 +114,58 @@
 (define-pari gmul (_fun _GEN _GEN -> _GEN))
 (define-pari gdiv (_fun _GEN _GEN -> _GEN))
 (define-pari gsqr (_fun _GEN -> _GEN))
+(define-pari factor (_fun _GEN -> _GEN))
 (define-pari gsqrtn (_fun _GEN _GEN (zetan : (_ptr o _GEN)) _realprec
                           -> (res : _GEN)
                           -> (values res zetan)))
 
-(define-pari factor (_fun _GEN -> _GEN))
+;; long u_lvalrem(ulong x, ulong y, ulong *r) "lUUU&":
+(define-pari u_lvalrem (_fun _ulong _ulong (r : (_ptr o _ulong))
+                             -> (res : _long)
+                             -> (values res r)))
+
+;; Some stack management functions:
+(define-pari gcopy (_fun _GEN -> _GEN))
+(define-pari gerepilecopy (_fun _pari_sp _GEN -> _GEN))
+(define-pari gerepileupto (_fun _pari_sp _GEN -> _GEN))
+
+;; GEN characteristic(GEN x) "mG":
+;; The 'm' means the function is 'shallow' in some sense; this is
+;; resolved by simply copying the result.
+(define-pari characteristic/raw (_fun _GEN -> _GEN)
+  #:c-id characteristic)
+(define (characteristic x) (gcopy (characteristic/raw x)))
+
+;; GEN member_j(GEN x) "mG" (the .j member function):
+(define-pari .j/raw (_fun _GEN -> _GEN)
+  #:c-id member_j)
+(define (.j x) (gcopy (.j/raw x)))
+
+;; Elliptic curves
+(define-pari ellfromj (_fun _GEN -> _GEN))
+;; FIXME: Need more general ellinit eventually.
+(define-pari ellinit (_fun _GEN (_GEN = gen_1) (_realprec = 3) -> _GEN))
+
+
+;; void untilpari(GEN E, GEN I) "vEI"
+;; N.B. We need to build the "entree*" from the given function
+;; pointer, which is then given to untilpari().
+(define-pari until (_fun (_fun -> _GEN) (_fun -> _void)
+                         -> _void)
+  #:c-id untilpari)
+;; void forprime(...) "vV=GDGI":
+;; We really want this to be used like so:
+;; (forprime [p 2 1000]
+;;   (let ([q (* p p)])
+;;     (display q)))
+;; in which the second argument is a progn.
+;; NB: Here the third parameter (the second GEN) is optional, but it
+;; is followed by a non-optional parameter with no default, so we will
+;; need to transform this somehow, since once a parameter has been
+;; made optional, all following parameters must also have defaults.
+;(define-pari forprime (_fun _variable _GEN _GEN (_fun -> _void)
+;                       -> void))
+
 
 (define (+ . args)
   (foldl gadd gen_0 args))
